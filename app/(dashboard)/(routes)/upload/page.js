fd94a40465328981de7c5937c9e2f07475ff0a5a
Alex Saxena <saxenaalex@gmail.com>
2024-05-16 16:13:45 +0200

    Adds h2 text for upload

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -3,7 +3,11 @@ import UploadForm from "./_components/UploadForm";
 
 function Upload() {
   return (
-    <div>
+    <div className="p-5 px-8 md:px-28">
+      <h2 className="text-[20px] text-center m-5 ">
+        Hoist the <strong className="text-primary">Files</strong>, Share the{" "}
+        <strong className="text-primary">Fortune</strong>!
+      </h2>
       <UploadForm />
     </div>
   );

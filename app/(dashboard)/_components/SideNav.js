0873cf946b35304ae87a64533404ad97e3fb5917
Alex Saxena <saxenaalex@gmail.com>
2024-05-16 15:00:27 +0200

    Updates SideNav to mpa menuList as btn

--- app/(dashboard)/_components/SideNav.js
+++ app/(dashboard)/_components/SideNav.js
@@ -4,16 +4,27 @@ import React from "react";
 
 function SideNav() {
   // List of Items to be in sidebar
-  const menulist = [
-    { id: 1, name: "upload", icon: Upload, path: "/upload" },
-    { id: 2, name: "files", icon: File, path: "/files" },
+  const menuList = [
+    { id: 1, name: "Upload", icon: Upload, path: "/upload" },
+    { id: 2, name: "Files", icon: File, path: "/files" },
     { id: 3, name: "WIP", icon: HardHat, path: "#" },
   ];
 
   return (
     <div>
-      <div className="o-5 border-b">
-        <Image src="/logo_chest.png" width={150} height={100} />
+      <div className="p-5 border-b">
+        <Image src="/logo_chest.png" width={150} height={100} alt="logo" />
+      </div>
+      <div className="flex flex-col float-left">
+        {menuList.map((item, index) => (
+          <button
+            key={item.id}
+            className="flex gap-2 p-4 px-6 hover:bg-gray-100 w-full text-gray-500"
+          >
+            <item.icon />
+            <h2>{item.name}</h2>
+          </button>
+        ))}
       </div>
     </div>
   );

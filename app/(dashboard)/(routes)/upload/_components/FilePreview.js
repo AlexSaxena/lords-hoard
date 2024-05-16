ab70909e581866dae7d204105a3a0d3aa032d75e
Alex Saxena <saxenaalex@gmail.com>
2024-05-16 18:06:43 +0200

    Adds file preview styling & functionality

--- app/(dashboard)/(routes)/upload/_components/FilePreview.js
+++ app/(dashboard)/(routes)/upload/_components/FilePreview.js
@@ -1,7 +1,20 @@
+import { File } from "lucide-react";
 import React from "react";
 
-function FilePreview() {
-  return <div>FilePreview</div>;
+function FilePreview({ file }) {
+  return (
+    <div className="flex items-center gap-3 justify-between mt-5 border rounded-md p-2 border-blue-300">
+      <div className="flex items-center p-2">
+        <File />
+        <div className="text-left pl-3">
+          <h2>{file.name}</h2>
+          <h2 className="text-[12px] text-gray-400">
+            {file.type} / {(file.size / 1024 / 1024).toFixed(2)}MB
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FilePreview;

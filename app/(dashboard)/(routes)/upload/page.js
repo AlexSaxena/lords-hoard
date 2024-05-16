ffd863a58ccdaa48a5a4256677d27fbc64ce9956
Alex Saxena <saxenaalex@gmail.com>
2024-05-16 20:56:12 +0200

    integrates firebase upload functionality

--- app/(dashboard)/(routes)/upload/page.js
+++ app/(dashboard)/(routes)/upload/page.js
@@ -1,10 +1,33 @@
 "use client";
 import React from "react";
 import UploadForm from "./_components/UploadForm";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "@/firebaseConfig";
 
 function Upload() {
+  const storage = getStorage(app);
+
   const uploadFile = (file) => {
-    console.log("page.js", file);
+    const metadata = {
+      contentType: file.type,
+    };
+    const storageRef = ref(storage, "file-upload/" + file?.name);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+    uploadTask.on("state_changed", (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+
+      progress === 100 &&
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+    });
   };
 
   return (

6d97787cd0233a4aba8cc10a63d6eeaa2c572d59
Alex Saxena <saxenaalex@gmail.com>
2024-05-16 16:27:28 +0200

    Updates drop zone styling

--- app/(dashboard)/(routes)/upload/_components/UploadForm.js
+++ app/(dashboard)/(routes)/upload/_components/UploadForm.js
@@ -6,11 +6,11 @@ function UploadForm() {
       <div className="flex items-center justify-center w-full">
         <label
           htmlFor="dropzone-file"
-          className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:hover:bg-gray-800 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500"
+          className="flex flex-col items-center justify-center w-full h-64 border-2 border-blue-300 border-dashed rounded-lg cursor-pointer bg-blue-50 hover:bg-gray-100 dark:hover:bg-gray-800 dark:bg-gray-700 dark:border-gray-600 dark:hover:border-gray-500"
         >
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
             <svg
-              className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+              className="w-12 h-12 mb-4 text-blue-500 dark:text-gray-400"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -24,9 +24,10 @@ function UploadForm() {
                 d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
               />
             </svg>
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and
-              drop
+            <p className="mb-2 text-lg md:text-2xl text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or{" "}
+              <strong className="text-primary">drag</strong> and
+              <strong className="text-primary"> drop</strong>
             </p>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               SVG, PNG, JPG or GIF (MAX. 800x400px)

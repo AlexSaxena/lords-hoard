ddd0a272da27b822de2d7801eec03808db8b6e60
Alex Saxena <saxenaalex@gmail.com>
2024-05-17 18:17:49 +0200

    Adds new page.js for dynamic file preview

---
+++ app/(dashboard)/(routes)/file-preview/[fileId]/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React, { useEffect } from "react";
+
+function FilePreview({ params }) {
+  useEffect(() => {
+    console.log(params?.fileId);
+  }, []);
+
+  return <div>FilePreview</div>;
+}
+
+export default FilePreview;

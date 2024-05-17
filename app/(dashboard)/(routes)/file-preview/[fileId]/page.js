36531e48d1f4c0155a68c71150d8ee09148139a9
Alex Saxena <saxenaalex@gmail.com>
2024-05-17 18:41:06 +0200

    Using firestore get file from database

--- app/(dashboard)/(routes)/file-preview/[fileId]/page.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/page.js
@@ -1,11 +1,27 @@
 "use client";
+import { app } from "@/firebaseConfig";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import React, { useEffect } from "react";
 
 function FilePreview({ params }) {
+  const db = getFirestore(app);
+
   useEffect(() => {
     console.log(params?.fileId);
+    params?.fileId && getFileInfo();
   }, []);
 
+  const getFileInfo = async () => {
+    const docRef = doc(db, "uploadedFile", params?.fileId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
   return <div>FilePreview</div>;
 }
 
